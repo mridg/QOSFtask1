fab8d82a6a6f978c6c4e933f748ef3d648ea9f16
moduloooo <86678091+mridg@users.noreply.github.com>
2024-11-08 15:21:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-#qubit 1 -> x gate -> cnot control
-#qubit 2 -> h gate -> cnot affect
-
 def Xgate(input):
     return input @ np.array([[0, 1], [1, 0]])
 
@@ -29,33 +26,65 @@ def createstatevector(n):
         statevector = np.kron(statevector, np.array([first, second]))
     return statevector
 
+def createtensor(n):
+    vec = createstatevector(n)
+    return np.reshape(vec, (2, -1))
+
 def matmulsimulation(n):
     #this is ssomething
     vec = createstatevector(n)
     for i in range (0, n * 2 - 4, 2):
-        vec[n : 2 + n] = Xgate(vec[n : 2 + n])
-        vec[n : 2 + n] = Hgate(vec[n : 2 + n])
-        vec[n : 4 + n] = CNOTgate(vec[n : 4 + n])
-    print(vec)
-    
+        vec[i : 2 + i] = Xgate(vec[i : 2 + i])
+        vec[i : 2 + i] = Hgate(vec[i : 2 + i])
+        vec[i : 4 + i] = CNOTgate(vec[i : 4 + i])
+    return vec 
 
-def runtimematmul():
-    #plot the runtime of your code as a function of the number of qubits. 
-    #what does this mean! 
-    bitarray = []
-    timearray = []
-    for i in range (3, 10):
-        bitarray.append(i)
+def runtime():
+    #plot the runtime of your code as a function of the number of qubits.
+    #what does this mean!
+    bitarraymatrix = []
+    timearraymatrix = []
+    bitarraytensor = []
+    timearraytensor = []
+    for i in range (3, 30):
+        bitarraymatrix.append(i)
         start = time.time()
         matmulsimulation(i)
         end = time.time()
-        timearray.append(end - start)
-    plt.plot(bitarray, timearray)
+        timearraymatrix.append(end - start)
     
+    for i in range (3, 30):
+        bitarraytensor.append(i)
+        start = time.time()
+        tensorsimulation(i)
+        end = time.time()
+        timearraytensor.append(end - start)
+
+    plt.plot(bitarraymatrix, timearraymatrix)
+    plt.title("time vs number of qubits for matrix")
+    plt.xlabel("number of qubits")
+    plt.ylabel("time taken (s)")
+    plt.show()
+
+    plt.plot(bitarraytensor, timearraytensor)
+    plt.title("time vs number of qubits for tensor")
+    plt.xlabel("number of qubits")
+    plt.ylabel("time taken (s)")
+    plt.show()
+
+def tensorsimulation(n):
+    vec = createtensor(n)
+    for i in range (0, len(vec) - 1):
+        vec[:, i] = Xgate(vec[:, i])
+        vec[:, i] = Hgate(vec[:, i])
+        cnotvec = np.kron(vec[:, i], vec[:, i + 1])
+        outvec = CNOTgate(cnotvec.T).T
+        vec[:, i] = outvec[0:1]
+        vec[:, i+1] = outvec[1:2]
+    return vec
 
 def main():
-    #idk something 
-    runtimematmul()
+    runtime()
 
 
 if __name__ == "__main__":
